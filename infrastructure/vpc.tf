177005b6e7954da61ed927e673c26a96396ebfd0
im-devendhar <devendharyadav781@gmail.com>
2026-03-01 11:12:59 +0530

    modified vpc.tf

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,29 +1,55 @@
 # Creating VPC
 
 resource "aws_vpc" "main"{
-    cidr_block = var.vpc_cidr
+    cidr_block          = var.vpc_cidr
+    enable_dns_support  = true
+    enable_dns_hostname = true
+
+    tag{
+        Name = "eks_vpc"
+    }
 }
 
 # Creating public subnets
 resource "aws_subnet" "public_subnet_1"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet_1_cidr
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.public_subnet_1_cidr
+    availability_zone = ${var.region}a
+
+    tags ={
+        Name = "public_subnet_1"
+    }
 }
 
 resource "aws_subnet" "public_subnet_2"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet_2_cidr
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.public_subnet_2_cidr
+    availability_zone = ${var.region}b
+
+    tags ={
+        Name = "public_subnet_2"
+    }
 }
 
 #Creating private subnets
 resource "aws_subnet" "private_subnet_1"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnet_1_cidr
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.private_subnet_1_cidr
+    availability_zone = ${var.region}a 
+
+    tags ={
+        Name = "private_subnet_1"
+    }
 }
 
 resource "aws_subnet" "private_subnet_2"{
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnet_2_cidr
+    vpc_id            = aws_vpc.main.id
+    cidr_block        = var.private_subnet_2_cidr
+    availability_zone = ${var.region}b
+
+    tags ={
+        Name = "private_subnet_2"
+    }
 }
 
 
@@ -39,20 +65,20 @@ resource "aws_route_table" "public_rt"{
 
 # Creating route for internet access in public route table
 resource "aws_route" "public_internet"{
-    route_table_id = aws_route_table.public_rt.id
+    route_table_id         = aws_route_table.public_rt.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id             = aws_internet_gateway.igw.id
 }
 
 # Associating public subnets with public route table
 
 resource "aws_route_table_association" "public_assoc_1"{
-   subnet_id = aws_subnet.public_subnet_1.id
+   subnet_id      = aws_subnet.public_subnet_1.id
    route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "public_assoc_2"{
-    subnet_id = aws_subnet.public_subnet_2.id
+    subnet_id      = aws_subnet.public_subnet_2.id
     route_table_id = aws_route_table.public_rt.id
 }
 
@@ -64,7 +90,7 @@ resource "aws_eip" "nat_eip"{
 # Creating nat gateway
 resource "aws_nat_gateway" "nat_gw"{
     allocation_id = aws_eip.nat_eip.id
-    subnet_id = aws_subnet.public_subnet_1.id
+    subnet_id     = aws_subnet.public_subnet_1.id
 
 }
 
