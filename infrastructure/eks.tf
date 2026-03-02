d59a48a51c226b6c182fb89fb8b4fed68078dd7d
im-devendhar <devendharyadav781@gmail.com>
2026-03-02 11:09:58 +0530

    modified vpc.tf

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -19,7 +19,7 @@ resource "aws_eks_node_group" "node_group"{
     node_group_name = "${var.cluster_name}-node-group"
     node_role_arn = aws_iam_role.eks_node_role.arn
 
-    subnet_ids[
+    subnet_ids = [
         aws_subnet.private_subnet_1.id,
         aws_subnet.private_subnet_1.id
     ]
--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main"{
 resource "aws_subnet" "public_subnet_1"{
     vpc_id            = aws_vpc.main.id
     cidr_block        = var.public_subnet_1_cidr
-    availability_zone = ${var.region}a
+    availability_zone = "${var.region}a"
 
     tags ={
         Name = "public_subnet_1"
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_subnet_1"{
 resource "aws_subnet" "public_subnet_2"{
     vpc_id            = aws_vpc.main.id
     cidr_block        = var.public_subnet_2_cidr
-    availability_zone = ${var.region}b
+    availability_zone = "${var.region}b"
 
     tags ={
         Name = "public_subnet_2"
@@ -35,7 +35,7 @@ resource "aws_subnet" "public_subnet_2"{
 resource "aws_subnet" "private_subnet_1"{
     vpc_id            = aws_vpc.main.id
     cidr_block        = var.private_subnet_1_cidr
-    availability_zone = ${var.region}a 
+    availability_zone = "${var.region}a"
 
     tags ={
         Name = "private_subnet_1"
